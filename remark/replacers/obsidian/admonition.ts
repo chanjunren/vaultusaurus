df41d04483c74d67580904b8f5b5852cb825783c
junren.chan <junren.chan@okg.com>
2024-08-13 20:06:43 +0800

    :sparkles: Implemented note-link

--- remark/replacers/obsidian/admonition.ts
+++ remark/replacers/obsidian/admonition.ts
@@ -1,11 +1,84 @@
-import { Find, Replace } from "mdast-util-find-and-replace";
+/** SAMPLE_NOTE
+ * 
+ {
+    "type": "containerDirective",
+    "name": "note",
+    "attributes": {},
+    "children": [
+        {
+            "type": "paragraph",
+            "children": [
+                {
+                    "type": "text",
+                    "value": "this is a note",
+                }
+            ],
+        }
+    ],
+},
+ */
 
-const admonitionReplacer: [Find, Replace?] = [
-  /code/gi,
-  function (value, capture, match) {
-    console.log("MATCHIE", match);
-    return ":::note\ntest\n:::";
-  },
-];
-
-export default admonitionReplacer;
+/**
+ * {
+    "type": "paragraph",
+    "children": [
+        {
+            "type": "text",
+            "value": "TIPPIE ",
+        },
+        {
+            "type": "strong",
+            "children": [
+                {
+                    "type": "text",
+                    "value": "content",
+                }
+            ],
+        },
+        {
+            "type": "text",
+            "value": " with ",
+        },
+        {
+            "type": "emphasis",
+            "children": [
+                {
+                    "type": "text",
+                    "value": "Markdown",
+                }
+            ],
+        },
+        {
+            "type": "text",
+            "value": " ",
+        },
+        {
+            "type": "inlineCode",
+            "value": "syntax",
+        },
+        {
+            "type": "text",
+            "value": ". Check ",
+        },
+        {
+            "type": "link",
+            "title": null,
+            "url": "#",
+            "children": [
+                {
+                    "type": "text",
+                    "value": "this ",
+                },
+                {
+                    "type": "inlineCode",
+                    "value": "api",
+                }
+            ],
+        },
+        {
+            "type": "text",
+            "value": ".",
+        }
+    ],
+}
+ */
--- remark/replacers/obsidian/index.ts
+++ remark/replacers/obsidian/index.ts
@@ -1,11 +1,12 @@
 import { Find, Replace } from "mdast-util-find-and-replace";
 import imageReplacer from "./img";
+import noteLinkReplacer from "./noteLink";
 import tagReplacer from "./tag";
 
 const OBSIDIAN_TRANSFORMERS: [Find, Replace?][] = [
   imageReplacer,
   tagReplacer,
-  // admonitionReplacer,
+  noteLinkReplacer,
 ];
 
 export default OBSIDIAN_TRANSFORMERS;
